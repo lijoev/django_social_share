071658877bb5bbcfd207a9fa1b0ac884b71eae51
Lijo <lijo.varghese@seqato.com>
2018-02-27 14:37:20 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'social_share_django',
+  packages = ['social_share_django'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A package for django social share',
+  author = 'Lijo Varghese',
+  author_email = 'lijov97@gmail.com',
+  url = 'https://spatterns@bitbucket.org/spatterns/django_social_share.git', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
